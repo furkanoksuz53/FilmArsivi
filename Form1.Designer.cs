3b8ed89072838db303abb08491a6cab64baf0002
furkanoksuz53 <furknoksuz34@gmail.com>
2023-07-17 15:39:27 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace FilmArsivi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.chromiumWebBrowser2 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbxStatu = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxPath = new System.Windows.Forms.TextBox();
+            this.tbxCategory = new System.Windows.Forms.TextBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.btnFullScreen = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(940, 222);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Film Listesi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(934, 201);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chromiumWebBrowser1);
+            this.groupBox2.Location = new System.Drawing.Point(276, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(679, 357);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ekran";
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(3, 18);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(673, 336);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            // 
+            // chromiumWebBrowser2
+            // 
+            this.chromiumWebBrowser2.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser2.Name = "chromiumWebBrowser2";
+            this.chromiumWebBrowser2.Size = new System.Drawing.Size(200, 100);
+            this.chromiumWebBrowser2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbxStatu);
+            this.groupBox3.Controls.Add(this.btnAdd);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.tbxPath);
+            this.groupBox3.Controls.Add(this.tbxCategory);
+            this.groupBox3.Controls.Add(this.tbxName);
+            this.groupBox3.Location = new System.Drawing.Point(12, 240);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(258, 186);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Film Ekle";
+            // 
+            // cbxStatu
+            // 
+            this.cbxStatu.AutoSize = true;
+            this.cbxStatu.Location = new System.Drawing.Point(143, 110);
+            this.cbxStatu.Name = "cbxStatu";
+            this.cbxStatu.Size = new System.Drawing.Size(18, 17);
+            this.cbxStatu.TabIndex = 3;
+            this.cbxStatu.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(64, 147);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(187, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Durum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Link";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kategori";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Film Adı";
+            // 
+            // tbxPath
+            // 
+            this.tbxPath.Location = new System.Drawing.Point(64, 81);
+            this.tbxPath.Name = "tbxPath";
+            this.tbxPath.Size = new System.Drawing.Size(188, 22);
+            this.tbxPath.TabIndex = 0;
+            // 
+            // tbxCategory
+            // 
+            this.tbxCategory.Location = new System.Drawing.Point(64, 53);
+            this.tbxCategory.Name = "tbxCategory";
+            this.tbxCategory.Size = new System.Drawing.Size(187, 22);
+            this.tbxCategory.TabIndex = 0;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(64, 25);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(187, 22);
+            this.tbxName.TabIndex = 0;
+            // 
+            // btnFullScreen
+            // 
+            this.btnFullScreen.Location = new System.Drawing.Point(12, 432);
+            this.btnFullScreen.Name = "btnFullScreen";
+            this.btnFullScreen.Size = new System.Drawing.Size(258, 23);
+            this.btnFullScreen.TabIndex = 2;
+            this.btnFullScreen.Text = "Tam Ekran";
+            this.btnFullScreen.UseVisualStyleBackColor = true;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(12, 461);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(258, 23);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.Text = "Renk Değiştir";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 490);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(258, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Çıkış";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FilmArsivi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(967, 603);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnFullScreen);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FilmArsivi";
+            this.Text = "Film Arşivi";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxPath;
+        private System.Windows.Forms.TextBox tbxCategory;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Button btnFullScreen;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox cbxStatu;
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser2;
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace FilmArsivi2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetFilms();
+            chromiumWebBrowser2.Load("https://www.google.com");
+            
+        }
+        public class Film
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Category { get; set; }
+            public string Path { get; set; }
+
+            public bool Statu { get; set; }
+
+        }
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FilmArsivi;";
+
+
+        public void GetFilms()
+        {
+            string query = "SELECT * FROM Films";
+            List<Film> films = new List<Film>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Film film = new Film()
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Category = reader.GetString(2),
+                            Path = reader.GetString(3),
+                            Statu = reader.GetBoolean(4)
+                        };
+                        films.Add(film);
+                    }
+                }
+
+                reader.Close();
+
+                dataGridView1.DataSource = films;
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (tbxCategory.Text.Length>0&& tbxName.Text.Length>0&& tbxPath.Text.Length>0)
+            {
+                try
+                {
+                    Film newFilm = new Film()
+                    {
+                        Category = tbxCategory.Text,
+                        Name = tbxName.Text,
+                        Path = tbxPath.Text,
+                        Statu = cbxStatu.Checked == true ? true : false
+                    };
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        string query = "INSERT INTO Films (Name, Category, Path, Statu) VALUES (@Name, @Category, @Path, @Statu)";
+
+                        SqlCommand command = new SqlCommand(query, connection);
+
+                        command.Parameters.AddWithValue("@Name", newFilm.Name);
+                        command.Parameters.AddWithValue("@Category", newFilm.Category);
+                        command.Parameters.AddWithValue("@Path", newFilm.Path);
+                        command.Parameters.AddWithValue("@Statu", newFilm.Statu);
+
+                        command.ExecuteNonQuery();
+
+                        connection.Close();
+                    }
+                    MessageBox.Show("Eklendi");
+                    GetFilms();
+
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun");
+            }
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selected = dataGridView1.SelectedCells[0].RowIndex;
+            string link = dataGridView1.Rows[selected].Cells[3].Value.ToString();
+            chromiumWebBrowser1.Load(link);
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public class Renk
+        {
+            public int Id { get; set; }
+            public string HtmlCode { get; set; }
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            List<Renk> colors = new List<Renk>();
+            Renk color1 = new Renk() { Id = 1, HtmlCode = "#FFFFFF" };
+            colors.Add(color1);
+            Renk color2 = new Renk() { Id = 2, HtmlCode = "#FF0000" };
+            colors.Add(color2);
+            Renk color3 = new Renk() { Id = 3, HtmlCode = "#00FF00" };
+            colors.Add(color3);
+            Renk color4 = new Renk() { Id = 4, HtmlCode = "#0000FF" };
+            colors.Add(color4);
+            Renk color5 = new Renk() { Id = 5, HtmlCode = "#D3D3D3" };
+            colors.Add(color5);
+            Renk color6 = new Renk() { Id = 6, HtmlCode = "#9ACD32" };
+            colors.Add(color6);
+            Renk color7 = new Renk() { Id = 7, HtmlCode = "#DA70D6" };
+            colors.Add(color7);
+            Random random = new Random();
+            int randomNumber = random.Next(1, 8);
+            Renk renk = colors.Where(x => x.Id == randomNumber).First();
+            BackColor = ColorTranslator.FromHtml(renk.HtmlCode);
+        }
+    }
+}
